bb60d0a4fe5ea4cabc5d3e7146c8359f619b000f
DaxInvader <DaxInvader@gmail.com>
2023-04-11 00:43:26 -0400

    final commit

--- live-feed2.js
+++ live-feed2.js
@@ -1,3 +1,4 @@
+const lastVideo = document.getElementById("lastVideo");
 const video = document.getElementById('videoFeed');
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
@@ -7,135 +8,48 @@ let segmentNumber = 1;
 let chunks = [];
 let uploadCounter = 0;
 
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => init());
 
-    // When a new video segment is ready
-    fetch('http://localhost:3000/getlatestvideo', {
-        method: 'GET',
-    })
-        .then((response) => response.text())
-        .then((result) => {
-            console.log('Download result:', result);
-        })
-        .catch((error) => {
-            console.error('Error downloading latest video segment:', error);
-        });
-});
+function init() {
+    window.addEventListener('load', async () => {
+        const filename = await getLatestVideo();
+        updateLatestVideo(filename);
+    });
 
+    startButton.addEventListener('click', start);
+    stopButton.addEventListener('click', stop);
+}
 
-
-startButton.addEventListener('click', () => {
-    // Call the /concatenate route here
-    fetch('http://localhost:3000/clearsegments', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then((result) => {
-            console.log('Clear segments result:', result);
-        })
-        .catch((error) => {
-            console.error('Error concat video segment:', error);
-        });
-
-    navigator.mediaDevices.getUserMedia({
+function createStream() {
+    return navigator.mediaDevices.getUserMedia({
         video: {
             width: 1280,
             height: 720,
             frameRate: { ideal: 30, max: 30 }
         },
         audio: true
-    })
-        .then(stream => {
-            video.srcObject = stream;
-            mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-            mediaRecorder.onstop = () => {
-                const waitForUploads = setInterval(() => {
-                    if (uploadCounter === 0) {
-                        clearInterval(waitForUploads);
-                        // Call the /concatenate route here
-                        fetch('http://localhost:3000/concatenate', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({}),
-                        })
-                            .then((response) => {
-                                if (!response.ok) {
-                                    throw new Error(`HTTP error! status: ${response.status}`);
-                                }
-                                return response.text();
-                            })
-                            .then((result) => {
-                                console.log('concat result:', result);
-                                compareList();
-                                getLatestVideo();
-                            })
-                            .catch((error) => {
-                                console.error('Error concat video segment:', error);
-                            });
-                    }
-                }, 100);
+    });
+}
 
-            };
-            mediaRecorder.ondataavailable = async (event) => {
-                const blob = new Blob([event.data], { type: 'video/mp4' });
-                const formData = new FormData();
-                formData.append('segment', blob, `segment${segmentNumber}.mp4`);
+function createMediaRecorder(stream) {
+    const mr = new MediaRecorder(stream, { mimeType: 'video/webm' });
+    mr.ondataavailable = mediaReceiverOnDataAvailable
+    mr.onstop = mediaReceiverOnStop
 
-                // Increment the upload counter
-                uploadCounter++
+    return mr;
+}
 
-                // When a new video segment is ready
-                fetch('http://localhost:3000/upload', {
-                    method: 'POST',
-                    body: formData
-                })
-                    .then((response) => response.text())
-                    .then((result) => {
-                        console.log('Upload result:', result);
-                        // Decrement the upload counter
-                        uploadCounter--;
-                    })
-                    .catch((error) => {
-                        console.error('Error uploading video segment:', error);
-                        // Decrement the upload counter
-                        uploadCounter--;
-                    });
-                //Upload data to mysql
-                fetch('upload.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                    .then(response => response.text())
-                    .then(result => {
-                        console.log('Upload result to MYSQL:', result);
-                    })
-                    .catch(error => {
-                        console.error('Error uploading video segment to MYSQL:', error);
-                    });
-                segmentNumber++;
-            };
-            mediaRecorder.start(3000);
+async function start() {
+    await clearSegments();
 
-        })
-        .catch(error => {
-            console.error('Error accessing camera:', error);
-        });
+    const stream = await createStream();
+    video.srcObject = stream;
 
-});
+    mediaRecorder = await createMediaRecorder(stream);
+    mediaRecorder.start(3000);
+}
 
-
-stopButton.addEventListener('click', () => {
+function stop() {
     if (mediaRecorder) {
         mediaRecorder.stop();
         chunks = [];
@@ -146,27 +60,88 @@ stopButton.addEventListener('click', () => {
         tracks.forEach(track => track.stop());
         video.srcObject = null;
     }
+}
 
-});
 
-//function to fetch the latest video
-async function getLatestVideo() {
-    try {
-        const response = await fetch('http://localhost:3000/getlatestvideo', { method: 'GET' });
-        const result = await response.text();
-        console.log('Download result:', result);
-        if (typeof onNewVideoUploaded === 'function') {
-            onNewVideoUploaded();
+async function mediaReceiverOnDataAvailable(event) {
+    const blob = new Blob([event.data], { type: 'video/mp4' });
+    const formData = new FormData();
+    formData.append('segment', blob, `segment${segmentNumber}.mp4`);
+
+    // Increment the upload counter
+    uploadCounter++
+
+    // When a new video segment is ready
+    await Promise.all([
+        fetch('http://localhost:3000/upload', { method: 'POST', body: formData })
+            .then((response) => response.text())
+            .then((response) => {
+                console.log(`Upload result to NodeJS: ${response}`);
+                uploadCounter--;
+            })
+            .catch((error) => {
+                console.error(`Error uploading video segment to NodeJS: ${error}`);
+                uploadCounter--;
+            }),
+
+        fetch('upload.php', { method: 'POST', body: formData })
+            .then((response) => response.text())
+            .then((response) => console.log(`Upload result to MYSQL: ${response}`))
+            .catch(error => console.error(`Error uploading video segment to MYSQL: ${error}`)),
+    ])
+        .catch((error) => console.error(error));
+
+    segmentNumber++;
+};
+
+function mediaReceiverOnStop() {
+    setTimeout(async () => {
+        if (uploadCounter === 0) {
+            const filename = await concatenate();
+            return updateLatestVideo(filename);
         }
-    } catch (error) {
-        console.error('Error downloading latest video segment:', error);
-    }
+    }, 1000);
 }
 
+async function updateLatestVideo(filename) {
+    console.log('setting latest video', filename);
 
-//function to fetch the latest video
-async function compareList() {
-
+    lastVideo.innerHTML = `<source src="./output/${filename}" type="video/mp4">`;
+    lastVideo.load();
+    lastVideo.style.display = "block";
 }
 
+function getLatestVideo() {
+    return fetch('http://localhost:3000/getlatestvideo', { method: 'GET' })
+        .then((response) => response.text())
+        .then((response) => {
+            const { filename } = JSON.parse(response)
+            return filename
+        })
+        .catch((error) => console.error('Error downloading latest video segment:', error));
+}
 
+function concatenate() {
+    return fetch('http://localhost:3000/concatenate', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: '{}',
+    })
+        .then((response) => response.text())
+        .then((response) => {
+            const { filename } = JSON.parse(response);
+            return filename
+        })
+        .catch((error) => console.error(`Error concatenate: ${error}`));
+}
+
+function clearSegments() {
+    return fetch('http://localhost:3000/clearsegments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: '{}',
+    })
+        .then((response) => response.text())
+        .then((response) => console.log(`Clear segments result: ${response}`))
+        .catch((error) => (!response.ok) && console.error(`Error clear segments: HTTP error! status: ${response.status}`));
+}
